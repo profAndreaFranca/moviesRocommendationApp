f5848421058405c2d9bee0b3c11ab01b5e602eee
dejandrea <dejandrea@gmail.com>
2023-04-14 00:08:10 -0300

    downgrade expo 47

--- components/TabNavigator.js
+++ components/TabNavigator.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PopularScreen from "../screens/Popular";
-import RecommendedScreen from "../screens/Recommendation";
+import RecommendationScreen from "../screens/Recommendation";
 import LikedScreen from "../screens/Liked";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RFValue } from "react-native-responsive-fontsize";
 
@@ -12,7 +12,7 @@ export default function TabNavigator() {
   return (
     <Tab.Navigator >
       <Tab.Screen name="Popular" component={PopularScreen} />
-      <Tab.Screen name="Recommended" component={RecommendedScreen} />
+      <Tab.Screen name="Recommended" component={RecommendationScreen} />
       <Tab.Screen name="Liked" component={LikedScreen} />
     </Tab.Navigator>
   );
